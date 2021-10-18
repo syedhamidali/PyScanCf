ca7ace88c633e07d23b70b6ea0306bd3deed5d2d
syedhamidali <hamidsyed37@gmail.com>
2021-10-18 20:10:40 +0530

    v 1.0.13

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="pyscancf",  # Required
-    version="1.0.12",  # Required
+    version="1.0.13",  # Required
     author='Syed Hamid Ali',
     description="Creates PyArt compatible cf-radial data from single scans of IMD Radar data",  # Optional
     long_description=long_description,  # Optional
@@ -29,7 +29,7 @@ setup(
     ],
     keywords="IMD Radar cf-radial single sweeps radar weather meteorology",  # Optional
     packages=find_packages(exclude=["contrib", "docs", "tests"]),  # Required
-    install_requires=["numpy","arm_pyart","netCDF4","cartopy","pandas"], # Required
+    install_requires=["numpy","arm_pyart","netCDF4","wradlib","pandas"], # Required
     project_urls={  # Optional
         "Bug Reports": "https://github.com/syedhamidali/PyScanCf/issues",
         "Source": "https://github.com/syedhamidali/PyScanCf/",
