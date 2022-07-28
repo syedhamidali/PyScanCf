1d3afa714eb70217acf8a413d0bf1ec3e2d3146c
syedhamidali <hamidsyed37@gmail.com>
2022-07-28 18:09:11 -0400

    all

--- pyscancf/__init__.py
+++ pyscancf/__init__.py
@@ -6,6 +6,6 @@ Top-level package (:mod:`pyscancf`)
 ==================================
 .. currentmodule:: pyscancf
 """
-from pyscancf import cfrad, get_grid, plot_cappi
+from .pyscancf import cfrad, get_grid, plot_cappi
 
 __all__ = [s for s in dir() if not s.startswith('_')]
